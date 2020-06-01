27302aa23f11e8fa83f85fc5be46f0ffecee95d1
Arthur Gabriel <arthurgmm516@hotmail.com>
2020-05-31 22:00:37 -0300

    Estilização de Listagem

--- project/src/pages/Registros/index.js
+++ project/src/pages/Registros/index.js
@@ -16,26 +16,23 @@ const Registros = () => {
       setList(temp);
     });
   });  
-
-  function ListViewItemSeparator(){
-    return (
-      <View style={{ height: 0.2, width: '100%', backgroundColor: '#808080' }} />
-    );    
-  }
   
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Registros</Text>
+      </View>      
       <FlatList 
         data={list}
-        ItemSeparatorComponent={ListViewItemSeparator}
+        
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
-          <View key={item.register_id} style={{ backgroundColor: 'white', padding: 20 }}>
-            <Text>Identificador: {item.register_identificador}</Text>
-            <Text>Data: {item.register_date}</Text>
-            <Text>Hora: {item.register_hour}</Text>
-            <Text>Latitude: {item.register_lat}</Text>
-            <Text>Longitude: {item.register_long}</Text>
+          <View key={item.register_id} style={styles.registerContainer}>
+            <Text style={styles.labelText}>Identificador: {item.register_identificador}</Text>
+            <Text style={styles.labelText}>Data: {item.register_date}</Text>
+            <Text style={styles.labelText}>Hora: {item.register_hour}</Text>
+            <Text style={styles.labelText}>Latitude: {item.register_lat}</Text>
+            <Text style={styles.labelText}>Longitude: {item.register_long}</Text>
           </View>
         )}        
       />
@@ -46,6 +43,29 @@ const Registros = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  header: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    padding: 10,
+    backgroundColor: '#FF7C7C',
+  },
+  title: {
+    fontSize: 23,
+    color: '#FFF',
+  },  
+  registerContainer: {
+    backgroundColor: '#FFF',
+    borderWidth: 5,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 10,
+  },
+  labelText: {
+    fontSize: 16,
+    fontWeight: 'bold',
   }
 });
 
